8cb30bc438d23819d074f7cba48dc6e951453368
geewey <geewey@gmail.com>
2019-11-18 10:59:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+let array = ["Ada", "Brendan", "Ali"]
+let eventName = "birthday"
+
+function writeCards(array, eventName) {
+    messages = []
+    for (let i = 0; i < array.length; i ++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    console.log(messages);
+}
+
+writeCards(array, eventName)
+
+
+let number = 10
+
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number);
+        number --;
+    }
+}
+
+countDown(number)
